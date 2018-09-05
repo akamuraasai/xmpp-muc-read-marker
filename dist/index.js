3b34cbdb6dcde72c899d3cb00c763dbcace733ec
Jonathan Willian <jonathan.willian.tod@gmail.com>
2018-09-05 03:06:46 -0300

    Minified the file

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,1 @@
-'use strict';module.exports=(client,stanzas,configs)=>{const NS='urn:xmpp:receipts';const readAttribute=stanzas.define({name:'read',element:'read',namespace:NS,fields:{id:stanzas.utils.attribute('id')}});stanzas.withMessage(Message=>{stanzas.extend(Message,readAttribute)})};
+'use strict';module.exports=function(client,stanzas,configs){var NS='urn:xmpp:receipts';var readAttribute=stanzas.define({name:'read',element:'read',namespace:NS,fields:{id:stanzas.utils.attribute('id')}});stanzas.withMessage(function(Message){stanzas.extend(Message,readAttribute)})};
