ba52475de771cc78ee8f1b22bd88bd4e5010151a
Jonathan Willian <jonathan.willian.tod@gmail.com>
2018-09-05 02:54:35 -0300

    Added compiled files.

---
+++ dist/index.js
@@ -0,0 +1,1 @@
+'use strict';module.exports=(client,stanzas,configs)=>{const NS='urn:xmpp:receipts';const readAttribute=stanzas.define({name:'read',element:'read',namespace:NS,fields:{id:stanzas.utils.attribute('id')}});stanzas.withMessage(Message=>{stanzas.extend(Message,readAttribute)})};
